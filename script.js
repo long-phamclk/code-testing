ee5494f2d026d698244e7d27d20fa987c047b978
chilikilong112 <chiliki.long@gmail.com>
2022-08-22 19:35:30 +1000

    abc

---
+++ script.js
@@ -0,0 +1,13 @@
+var personName;
+console.log('My name is');
+
+var pets;
+console.log('I have');
+console.log('pet');
+
+var funFact;
+console.log('Fun fact:');
+
+personName = 'Long';
+pets = '2';
+funFact = 'lala';
